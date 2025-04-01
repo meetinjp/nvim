9228578d96719429fecdeccf4488796983d137cc
meetinjp <meetinjp@proton.me>
2025-04-01 19:39:28 +0200

    feat(markdown_preview): install w/ npm

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,15 +1,13 @@
 return {
-	-- install without yarn or npm
+	-- install or npm
 	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
 		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		config = function()
-			vim.g.mkdp_auto_close = 0
-		end,
 		keys = {
 			{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>" },
 		},
