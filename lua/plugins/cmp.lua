e8f0c3b47bb444263354047d8b733e2d0aa4441b
Kacper Świrkosz <kacper.swirkosz@lunarlogic.io>
2026-03-23 23:18:52 +0100

    feat: update cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,15 +4,28 @@ return {
 	{ "hrsh7th/cmp-nvim-lua" },
 	{ "saadparwaiz1/cmp_luasnip" },
 	{
-
 		"hrsh7th/nvim-cmp",
 		opts = {
 			sources = {
-				{ name = "path" },
+				{
+					name = "path",
+					option = {
+						trailing_slash = true,
+						label_trailing_slash = true,
+					},
+				},
+				{
+					name = "buffer",
+					option = {
+						get_bufnrs = function()
+							-- TODO: Get all buffers from harpoon, including the current one.
+							return { vim.api.nvim_get_current_buf() }
+						end,
+					},
+				},
+				{ name = "nvim_lua" },
 				{ name = "luasnip" },
 				{ name = "nvim_lsp" },
-				{ name = "nvim_lua" },
-				{ name = "buffer" },
 			},
 			snippet = {
 				expand = function(args)
