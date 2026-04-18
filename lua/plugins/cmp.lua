2e0ef1605a88b532a8606ba9f6c7a5ca9f479621
meetinjp <meetinjp@proton.me>
2026-04-18 23:40:54 +0200

    feat: update cmp keybindings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,6 +6,70 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		opts = {
+			mapping = {
+				["<Tab>"] = {
+					i = function(fallback)
+						local cmp = require("cmp")
+						local luasnip = require("luasnip")
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end,
+					s = function(fallback)
+						local luasnip = require("luasnip")
+						if luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end,
+				},
+				["<S-Tab>"] = {
+					i = function(fallback)
+						local cmp = require("cmp")
+						local luasnip = require("luasnip")
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end,
+					s = function(fallback)
+						local luasnip = require("luasnip")
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end,
+				},
+				["<CR>"] = {
+					i = function(fallback)
+						local cmp = require("cmp")
+						if cmp.visible() and cmp.get_selected_entry() then
+							cmp.confirm({ select = false })
+						else
+							fallback()
+						end
+					end,
+				},
+				["<C-Space>"] = {
+					i = function()
+						require("cmp").complete()
+					end,
+				},
+				["<C-e>"] = {
+					i = function()
+						require("cmp").abort()
+					end,
+				},
+			},
 			sources = {
 				{
 					name = "path",
@@ -18,7 +82,6 @@ return {
 					name = "buffer",
 					option = {
 						get_bufnrs = function()
-							-- TODO: Get all buffers from harpoon, including the current one.
 							return { vim.api.nvim_get_current_buf() }
 						end,
 					},
